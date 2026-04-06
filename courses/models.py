cae57c01ca384604a228628c12ac9326c186fb67
Hruaia <hruaitluangasailo20@gmail.com>
2026-04-06 07:35:31 +0530

    display

--- courses/models.py
+++ courses/models.py
@@ -67,6 +67,7 @@ class Subject(models.Model):
         blank=True,
         null=True
     )
+    order = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
